8b3a7508ba02a37a8944684415268667ea1726ea
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-04-07 00:07:10 -0700

    chore: improve user experience with better menu

--- src/app/notebook/notebook.module.ts
+++ src/app/notebook/notebook.module.ts
@@ -5,13 +5,14 @@ import { NotebookRoutingModule } from './notebook-routing.module';
 import { NotebookComponent } from './notebook.component';
 import {JsonFormsModule} from "@jsonforms/angular";
 import { FormComponent } from './form/form.component';
-import {MAT_TOOLTIP_SCROLL_STRATEGY_FACTORY_PROVIDER} from "@angular/material/tooltip";
-import {MAT_SELECT_SCROLL_STRATEGY_PROVIDER} from "@angular/material/select";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {MatButtonModule} from "@angular/material/button";
 import {MatRadioModule} from "@angular/material/radio";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import { JsonFormsAngularMaterialModule } from '@jsonforms/angular-material';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -24,6 +25,9 @@ import { JsonFormsAngularMaterialModule } from '@jsonforms/angular-material';
     JsonFormsModule,
     JsonFormsAngularMaterialModule,
     MatAutocompleteModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
     MatButtonModule,
     MatRadioModule,
     MatSnackBarModule
